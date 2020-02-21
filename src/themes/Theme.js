daac5b4a367f6f3df20b004b6deaa5ad0fe641d4
Byron Hill <byron@cautioncreation.com>
2020-02-21 01:03:11 -0700

    fixed button focus

--- src/themes/Theme.js
+++ src/themes/Theme.js
@@ -55,6 +55,10 @@ const GlobalStyle = createGlobalStyle`
 		box-sizing: border-box;
 	}
 
+	input:focus, textarea:focus, select:focus, button:focus{
+        outline: none;
+  }
+
 	h1, h2, h3, h4, h5, h6 {
 		font-family: ${props => props.theme.fonts.primary};
 		color: ${props => props.theme.colors.black};
