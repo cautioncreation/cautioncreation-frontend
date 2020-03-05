d84cc662abccd52953bafce49f3d94d13c290fe4
Byron Hill <byron@cautioncreation.com>
2020-03-05 16:56:32 -0700

    fixed servicesDescriptino routes'

--- src/components/servicesDescription/index.js
+++ src/components/servicesDescription/index.js
@@ -28,7 +28,7 @@ export const ServicesDescription = ({ src }) => (
 				<StyledParagraph>
 					Our experienced development team has the experience to ensure that your website or application has the speed, simplicity and style to increase web traffic, conversions and every other metric important to your businesses success. We understand not only your businesses needs, but the needs of your customers as well, and it shows in our development practices.
 				</StyledParagraph>
-				<Button href="/services/development">Learn More</Button>
+				<Button href="/development">Learn More</Button>
 			</StyledColumn>
 			<Column xs="12" lg="4">
 				<StyledVectorWrapper>
@@ -49,7 +49,7 @@ export const ServicesDescription = ({ src }) => (
 				<StyledParagraphRight>
 					In today's digital world, brand identity matters. We work hard to ensure that when new visitors view your site they not only have a smooth experience, but they remember your brand identity as well. We do this by ensuring not only that the colours, fonts and logos match your brand, but that the imagery and content speak to your comapanies vision as well.
 				</StyledParagraphRight>
-				<Button href="/services/branding-and-design">Learn More</Button>
+				<Button href="/branding-and-design">Learn More</Button>
 			</StyledColumnRight>
 			<Column xs="0" lg="1"/>
 		</StyledRow>
@@ -60,7 +60,7 @@ export const ServicesDescription = ({ src }) => (
 				<StyledParagraph>
 					Keeping your websites content and security updated can be a huge undertaking. In a lot of cases it can impede on your ability to run your business, especially if you aren't particularily web literate. Thankfully, Caution Creation is here to step in and take over. Content, security, backups, updates, SEO and more. No matter the size or complexity, we've got your digital needs covered.
 				</StyledParagraph>
-				<Button href="/services/maintenance">Learn More</Button>
+				<Button href="/maintenance">Learn More</Button>
 			</StyledColumn>
 			<Column xs="12" lg="4">
 				<StyledVectorWrapper>
