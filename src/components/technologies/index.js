54374f3144c48536872f4d91beaa7fec2477dd0e
Byron Hill <byron@cautioncreation.com>
2020-03-07 18:58:26 -0700

    converting imports to queries for img to increase loading speeds

--- src/components/technologies/index.js
+++ src/components/technologies/index.js
@@ -1,16 +1,6 @@
 import React from 'react'
 import { withTheme } from 'styled-components'
-
-import sass from '../../images/svg/sass.svg'
-import nodejs from '../../images/svg/nodejs.svg'
-import mysql from '../../images/svg/mysql.svg'
-import react from '../../images/svg/react.svg'
-import php from '../../images/svg/php.svg'
-import gatsby from '../../images/svg/gatsby.svg'
-import heroku from '../../images/svg/heroku.svg'
-import strapi from '../../images/svg/strapi.svg'
-import netlify from '../../images/svg/netlify.svg'
-import bootstrap from '../../images/svg/bootstrap.svg'
+import { useStaticQuery, graphql } from 'gatsby'
 
 import { ContainerStatic, Row, Column } from '../grid'
 import { Slider } from '../slider'
@@ -22,46 +12,83 @@ import {
 	StyledVector,
 } from './style'
 
-export const Technologies = withTheme(({ theme }) => (
-	<ContainerStatic background={theme.colors.lightGrey}>
-		<StyledTitle>A Few of Our Favourite Technologies</StyledTitle>
-		<Row>
-			<Column xs="0" lg="1" spacer />
-			<StyledColumn xs="12" lg="10">
-				<Slider interval="2" prefix="technologies" show="5" hideIcons>
-					<StyledVectorContainer>
-						<StyledVector src={sass} alt="Sass logo" />
-					</StyledVectorContainer>
-					<StyledVectorContainer>
-						<StyledVector src={nodejs} alt="Node.js logo" />
-					</StyledVectorContainer>
-					<StyledVectorContainer>
-						<StyledVector src={mysql} alt="mySQL logo" />
-					</StyledVectorContainer>
-					<StyledVectorContainer>
-						<StyledVector src={react} alt="React.js logo" />
-					</StyledVectorContainer>
-					<StyledVectorContainer>
-						<StyledVector src={php} alt="PHP logo" />
-					</StyledVectorContainer>
-					<StyledVectorContainer>
-						<StyledVector src={gatsby} alt="Gatsby.js logo" />
-					</StyledVectorContainer>
-					<StyledVectorContainer>
-						<StyledVector src={netlify} alt="Netlify logo" />
-					</StyledVectorContainer>
-					<StyledVectorContainer>
-						<StyledVector src={strapi} alt="Strapi logo" />
-					</StyledVectorContainer>
-					<StyledVectorContainer>
-						<StyledVector src={heroku} alt="Heroku logo" />
-					</StyledVectorContainer>
-					<StyledVectorContainer>
-						<StyledVector src={bootstrap} alt="Bootstrap logo" />
-					</StyledVectorContainer>
-				</Slider>
-			</StyledColumn>
-			<Column xs="0" lg="1" spacer />
-		</Row>
-	</ContainerStatic>
-))
+export const Technologies = withTheme(({ theme }) => {
+	const data = useStaticQuery(graphql`
+    query technologiesQuery {
+			sass:file(relativePath: { eq: "svg/sass.svg" }) {
+	      publicURL
+	    }
+			nodejs:file(relativePath: { eq: "svg/nodejs.svg" }) {
+	      publicURL
+	    }
+			mysql:file(relativePath: { eq: "svg/mysql.svg" }) {
+	      publicURL
+	    }
+			react:file(relativePath: { eq: "svg/react.svg" }) {
+	      publicURL
+	    }
+			php:file(relativePath: { eq: "svg/php.svg" }) {
+	      publicURL
+	    }
+			gatsby:file(relativePath: { eq: "svg/gatsby.svg" }) {
+	      publicURL
+	    }
+			heroku:file(relativePath: { eq: "svg/heroku.svg" }) {
+	      publicURL
+	    }
+			strapi:file(relativePath: { eq: "svg/strapi.svg" }) {
+	      publicURL
+	    }
+			netlify:file(relativePath: { eq: "svg/netlify.svg" }) {
+	      publicURL
+	    }
+			bootstrap:file(relativePath: { eq: "svg/bootstrap.svg" }) {
+	      publicURL
+	    }
+    }
+  `)
+
+	return (
+		<ContainerStatic background={theme.colors.lightGrey}>
+			<StyledTitle>A Few of Our Favourite Technologies</StyledTitle>
+			<Row>
+				<Column xs="0" lg="1" spacer />
+				<StyledColumn xs="12" lg="10">
+					<Slider interval="2" prefix="technologies" show="5" hideIcons>
+						<StyledVectorContainer>
+							<StyledVector fluid={data.sass.publicURL} alt="Sass logo" />
+						</StyledVectorContainer>
+						<StyledVectorContainer>
+							<StyledVector fluid={data.nodejs.publicURL} alt="Node.js logo" />
+						</StyledVectorContainer>
+						<StyledVectorContainer>
+							<StyledVector fluid={data.mysql.publicURL} alt="mySQL logo" />
+						</StyledVectorContainer>
+						<StyledVectorContainer>
+							<StyledVector fluid={data.react.publicURL} alt="React.js logo" />
+						</StyledVectorContainer>
+						<StyledVectorContainer>
+							<StyledVector fluid={data.php.publicURL} alt="PHP logo" />
+						</StyledVectorContainer>
+						<StyledVectorContainer>
+							<StyledVector fluid={data.gatsby.publicURL} alt="Gatsby.js logo" />
+						</StyledVectorContainer>
+						<StyledVectorContainer>
+							<StyledVector fluid={data.netlify.publicURL} alt="Netlify logo" />
+						</StyledVectorContainer>
+						<StyledVectorContainer>
+							<StyledVector fluid={data.strapi.publicURL} alt="Strapi logo" />
+						</StyledVectorContainer>
+						<StyledVectorContainer>
+							<StyledVector fluid={data.heroku.publicURL} alt="Heroku logo" />
+						</StyledVectorContainer>
+						<StyledVectorContainer>
+							<StyledVector fluid={data.bootstrap.publicURL} alt="Bootstrap logo" />
+						</StyledVectorContainer>
+					</Slider>
+				</StyledColumn>
+				<Column xs="0" lg="1" spacer />
+			</Row>
+		</ContainerStatic>
+	)
+})
--- src/components/technologies/style.js
+++ src/components/technologies/style.js
@@ -1,5 +1,5 @@
 import styled from 'styled-components'
-
+import Image from "gatsby-image"
 import { StyledColumn as Column } from '../grid/style'
 
 export const StyledColumn = styled(Column)`
